f75c7c16d8d420210fa585162b51f72f5b0d9a57
Mariia Tigina <tiginamaria@google.com>
2020-08-05 13:24:01 +0300

    Add heart rate service implementation

---
+++ server/app/src/main/java/com/google/heartrate/wearos/app/gatt/attributes/GattCharacteristic.java
@@ -0,0 +1,240 @@
+package com.google.heartrate.wearos.app.gatt.attributes;
+
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCharacteristic;
+
+import com.google.heartrate.wearos.app.gatt.GattException;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.UUID;
+
+/**
+ * {@link GattCharacteristic} is the wrapper over {@link BluetoothGattCharacteristic} class.
+ * <p>
+ * {@link GattCharacteristic} provides method which are common for all GATT characteristics.
+ * This abstraction makes all operations with {@link BluetoothGattCharacteristic}
+ * expandable for each specific characteristic type.
+ */
+public class GattCharacteristic implements GattAttribute {
+
+    /** {@link BluetoothGattCharacteristic} for characteristic. */
+    private final BluetoothGattCharacteristic mBluetoothGattCharacteristic;
+
+    /** Descriptors which characteristic include. */
+    private final HashMap<UUID, GattDescriptor> descriptorByUuid = new HashMap<>();
+
+    /**
+     * Configure {@link BluetoothGattCharacteristic} with given parameters.
+     * @param uuid characteristic uuid
+     * @param properties characteristic properties
+     * @param permissions characteristic permissions
+     */
+    public GattCharacteristic(UUID uuid, int properties, int permissions) {
+        mBluetoothGattCharacteristic =
+                new BluetoothGattCharacteristic(uuid, properties, permissions);
+    }
+
+    /**
+     * Configure {@link BluetoothGattCharacteristic} with given parameters and descriptors.
+     *
+     * @param uuid characteristic uuid
+     * @param properties characteristic properties
+     * @param permissions characteristic permissions
+     * @param gattDescriptors characteristic descriptors
+     */
+    public GattCharacteristic(UUID uuid, int properties, int permissions, GattDescriptor[] gattDescriptors) {
+        this(uuid, properties, permissions);
+
+        for (GattDescriptor gattDescriptor : gattDescriptors) {
+            descriptorByUuid.put(gattDescriptor.getUUid(), gattDescriptor);
+            mBluetoothGattCharacteristic.addDescriptor(gattDescriptor.getBluetoothGattDescriptor());
+        }
+    }
+
+    /**
+     * Determine weather characteristic has descriptor with given uuid or not.
+     *
+     * @param uuid descriptor uuid to check
+     * @return true if characteristic has descriptor, false otherwise
+     */
+    public boolean hasDescriptor(UUID uuid) {
+        return descriptorByUuid.containsKey(uuid);
+    }
+
+    /**
+     * Get {@link GattDescriptor} for descriptor in characteristic with given uuid.
+     *
+     * @param descriptorUuid descriptor uuid
+     * @return {@link GattDescriptor} for characteristic
+     * @throws GattException if characteristic does not support descriptor with given uuid
+     */
+    public GattDescriptor getDescriptor(UUID descriptorUuid) throws GattException {
+        GattDescriptor descriptorManager = descriptorByUuid.get(descriptorUuid);
+        if (descriptorManager == null) {
+            throw new GattException(String.format("Characteristic %s does not have descriptor %s",
+                    getUUid(), descriptorUuid));
+        }
+        return descriptorManager;
+    }
+
+    /**
+     * Process default read characteristic request.
+     * If special actions need to be done on read request, please override this method.
+     *
+     * @param device device from which read request was received
+     * @param offset value offset
+     * @return current value of characteristic
+     * @throws GattException if value can not be read
+     */
+    public byte[] read(BluetoothDevice device, int offset) throws GattException {
+        // TODO: implement getValue with offset, not required for now
+        return getValue();
+    }
+
+    /**
+     * Process default write characteristic request.
+     * If special actions need to be done on write request, please override this method.
+     *
+     * @param device device from which write request was received
+     * @param offset value offset
+     * @throws GattException if value can not be write
+     */
+    public void write(BluetoothDevice device, int offset, byte[] value) throws GattException {
+        // TODO: implement setValue with offset, not required for now
+        setValue(value);
+    }
+
+    /**
+     * Get value from characteristic.
+     *
+     * @return value characteristic
+     * @throws GattException if value can not be got
+     */
+    protected byte[] getValue() throws GattException {
+        try {
+            byte[] value = mBluetoothGattCharacteristic.getValue();
+            if (value == null) {
+                throw new GattException(String.format("Value can not be " +
+                        "got from characteristic %s.", getUUid()));
+            }
+            return value;
+        } catch (Exception e) {
+            throw new GattException(e);
+        }
+    }
+
+    /**
+     * Get int value from characteristic.
+     *
+     * @param format format at which the value should be get
+     * @param offset offset at which the value should be get
+     * @return int value in given format
+     * @throws GattException if value can not be got
+     */
+    protected int getIntValue(int format, int offset) throws GattException {
+        try {
+            Integer value = mBluetoothGattCharacteristic.getIntValue(format, offset);
+            if (value == null) {
+                throw new GattException(String.format("Value in format %d with offset %d can not be " +
+                        "got from characteristic %s.", format, offset, getUUid()));
+            }
+            return value;
+        } catch (Exception e) {
+            throw new GattException(e);
+        }
+    }
+
+    /**
+     * Set int value to characteristic.
+     *
+     * @param value  value to set
+     * @param format format at which the value should be set
+     * @param offset offset at which the value should be set
+     * @throws GattException if value can not be set
+     */
+    protected void setIntValue(int value, int format, int offset) throws GattException {
+        if (!mBluetoothGattCharacteristic.setValue(value, format, offset)) {
+            throw new GattException(String.format("Value %d in format %d with offset %d has not been " +
+                    "set to characteristic %s.", value, format, offset, getUUid()));
+        }
+    }
+
+    /**
+     * Set value to characteristic.
+     *
+     * @param value value to set
+     * @throws GattException if value can not be set
+     */
+    protected void setValue(byte[] value) throws GattException {
+        if (!mBluetoothGattCharacteristic.setValue(value)) {
+            throw new GattException(String.format("Value %s can not be " +
+                    "set to characteristic %s.", Arrays.toString(value), getUUid()));
+        }
+    }
+
+    /**
+     * Assert characteristic has read permissions.
+     *
+     * @throws GattException when characteristic has no read permissions
+     */
+    public void assertCharacteristicReadable() throws GattException {
+        if ((mBluetoothGattCharacteristic.getProperties() &
+                (BluetoothGattCharacteristic.PROPERTY_READ)) == 0) {
+            throw new GattException(String.format("Characteristic %s has no read permissions", getUUid()),
+                    BluetoothGatt.GATT_READ_NOT_PERMITTED);
+        }
+    }
+
+    /**
+     * Assert characteristic has write permissions.
+     *
+     * @throws GattException when characteristic has no write permissions
+     */
+    public void assertCharacteristicWritable() throws GattException {
+        if ((mBluetoothGattCharacteristic.getProperties() &
+                (BluetoothGattCharacteristic.PROPERTY_WRITE |
+                        BluetoothGattCharacteristic.PROPERTY_WRITE_NO_RESPONSE)) == 0) {
+            throw new GattException(String.format("Characteristic %s has no write permissions", getUUid()),
+                    BluetoothGatt.GATT_WRITE_NOT_PERMITTED);
+        }
+    }
+
+    /**
+     * Assert characteristic has notify permissions.
+     *
+     * @throws GattException when characteristic has no notify permissions
+     */
+    public void assertCharacteristicNotifiable() throws GattException {
+        if ((mBluetoothGattCharacteristic.getProperties() &
+                BluetoothGattCharacteristic.PROPERTY_NOTIFY) == 0) {
+            throw new GattException(String.format("Characteristic %s has no write permissions", getUUid()),
+                    BluetoothGatt.GATT_REQUEST_NOT_SUPPORTED);
+        }
+    }
+
+    /**
+     * Determine weather characteristic requires write response.
+     *
+     * @return true if property is writable without response, false otherwise
+     */
+    public boolean isWriteResponseRequired() {
+        return (mBluetoothGattCharacteristic.getProperties() &
+                BluetoothGattCharacteristic.PROPERTY_WRITE_NO_RESPONSE) == 0;
+    }
+
+    /**
+     * Get {@link BluetoothGattCharacteristic} for characteristic.
+     *
+     * @return {@link BluetoothGattCharacteristic} for characteristic
+     */
+    public BluetoothGattCharacteristic getBluetoothGattCharacteristic() {
+        return mBluetoothGattCharacteristic;
+    }
+
+    @Override
+    public UUID getUUid() {
+        return mBluetoothGattCharacteristic.getUuid();
+    }
+}
---
+++ server/app/src/main/java/com/google/heartrate/wearos/app/gatt/heartrate/characteristics/HeartRateMeasurementCharacteristic.java
@@ -0,0 +1,269 @@
+package com.google.heartrate.wearos.app.gatt.heartrate.characteristics;
+
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.util.Log;
+
+import com.google.heartrate.wearos.app.gatt.FormatUtils;
+import com.google.heartrate.wearos.app.gatt.GattException;
+import com.google.heartrate.wearos.app.gatt.attributes.GattCharacteristic;
+import com.google.heartrate.wearos.app.gatt.attributes.GattDescriptor;
+import com.google.heartrate.wearos.app.gatt.heartrate.descriptors.ClientCharacteristicConfigurationDescriptor;
+
+import java.util.UUID;
+
+/**
+ * {@link HeartRateMeasurementCharacteristic} class inherits {@link GattCharacteristic}
+ * and specifies set/get operation for Heart Rate Measurement Characteristic characteristic.
+ * <p>
+ * Fields included in the characteristic:
+ * <table>
+ *  <thead>
+ *      <tr><th>Field</th><th>Requirement</th><th>Format</th></tr>
+ *  <thead>
+ *  <tbody>
+ *      <tr><td> Flags </td><td> Mandatory </td><td> UInt8 </td></tr>
+ *      <tr><td> Heart Rate Measurement </td><td> Mandatory </td><td> UInt8/UInt16 </td></tr>
+ *      <tr><td> Energy Expended </td><td> Optional, Mandatory if Energy Expended flag </td><td> UInt16 </td></tr>
+ *      <tr><td> RR-Interval </td><td> Optional, Not supported </td><td> - </td></tr>
+ *      <tr><td> Transmission Interval </td><td> Optional, Not supported </td><td> - </td></tr>
+ *  </tbody>
+ * </table>
+ * <p>
+ * See <a href="https://www.bluetooth.com/wp-content/uploads/Sitecore-Media-Library/Gatt/Xml/Characteristics/org.bluetooth.characteristic.heart_rate_measurement.xml".>
+ * Heart Rate Measurement Characteristic</a>.
+ */
+public class HeartRateMeasurementCharacteristic extends GattCharacteristic {
+    private static final String TAG = HeartRateMeasurementCharacteristic.class.getCanonicalName();
+
+    /** Heart Rate Measurement characteristic UUID. */
+    private static final UUID HEART_RATE_MEASUREMENT_UUID = UUID
+            .fromString("00002a37-0000-1000-8000-00805f9b34fb");
+
+    /** Indicates that the data format of the Heart Rate Measurement value field is in a format of UINT8.  */
+    private static final byte HEART_RATE_MEASUREMENT_UINT8_FLAG = 0b00000000;
+
+    /** Indicates that the data format of the Heart Rate Measurement value field is in a format of UINT16. */
+    private static final byte HEART_RATE_MEASUREMENT_UINT16_FLAG = 0b00000001;
+
+    /** Indicates that the Energy Expended field is present in the Heart Rate Measurement characteristic. */
+    private static final byte EXPENDED_ENERGY_FLAG = 0b00001000;
+
+
+    /** Offset at which the Heart Rate Measurement characteristic flags are stored. */
+    private static final int FLAGS_OFFSET = 0;
+
+    /** Offset at which Heart Rate Measurement value is stored. */
+    private static final int HEART_RATE_MEASUREMENT_OFFSET = 1;
+
+    /** Offset at which Expended Energy value is Heart Rate Measurement value field is in a format of UINT8. */
+    private static final int EXPENDED_ENERGY_BASE_OFFSET = 2;
+
+    /** Offset at which Expended Energy value is Heart Rate Measurement value field is in a format of UINT16. */
+    private static final int EXPENDED_ENERGY_SHIFTED_OFFSET = 3;
+
+    /**
+     * Create {@link GattCharacteristic} for Heart Rate Measurement characteristic.
+     * <br>Configure {@link GattCharacteristic} with Heart Rate Measurement characteristic UUID,
+     * property for notification, no read/write permissions and Client Characteristic Configuration Descriptor.
+     */
+    public HeartRateMeasurementCharacteristic() {
+        super(HEART_RATE_MEASUREMENT_UUID,
+                BluetoothGattCharacteristic.PROPERTY_NOTIFY,
+                BluetoothGattCharacteristic.PROPERTY_NOTIFY,
+                new GattDescriptor[] {new ClientCharacteristicConfigurationDescriptor()});
+    }
+
+    /**
+     * Get heart rate measurement value.
+     *
+     * @return heart rate measurement value
+     * @throws GattException in case of wrong format or offset for heart rate measurement value
+     */
+    public int getHeartRateMeasurementValue() throws GattException {
+        Log.i(TAG, "Get heart rate measurement value");
+
+        int format = isUInt16HeartRateFormat() ?
+                BluetoothGattCharacteristic.FORMAT_UINT16 :
+                BluetoothGattCharacteristic.FORMAT_UINT8;
+
+        int heartRateMeasurement = getIntValue(format, HEART_RATE_MEASUREMENT_OFFSET);
+
+        Log.v(TAG, String.format("Heart rate format=%d value=%d", format, heartRateMeasurement));
+
+        return heartRateMeasurement;
+    }
+
+    /**
+     * Get expended energy value.
+     *
+     * @return expended energy value
+     * @throws GattException if expended energy is not presented or in case of wrong format or offset for expended energy value
+     */
+    public int getExpandedEnergyValue() throws GattException {
+        Log.i(TAG, "Get expended energy value");
+
+        assertExpandedEnergyIsPresented();
+
+        /* HR is in UInt8 format => EE offset is base */
+        /* HR is in UInt16 format => EE offset is shifted */
+        int expendedEnergyOffset = isUInt16HeartRateFormat() ?
+                EXPENDED_ENERGY_SHIFTED_OFFSET :
+                EXPENDED_ENERGY_BASE_OFFSET;
+
+        int expendedEnergy = getIntValue(
+                BluetoothGattCharacteristic.FORMAT_UINT16,
+                expendedEnergyOffset);
+
+        Log.v(TAG, String.format("Expended energy offset=%d value=%d", expendedEnergyOffset, expendedEnergy));
+
+        return expendedEnergy;
+    }
+
+    /**
+     * Set given Heart Rate Measurement and Expended Energy values to the characteristic.
+     * <p>
+     * Flags 8bit format 0b00000000 from right to left:
+     * <br>Heart Rate Format (0) -> UInt8 Format, (1) -> UInt16 Format
+     * <br>Sensor Contact Status (00) -> Not Supported
+     * <br>Energy Expended (1) -> Field Present
+     * <br>RR-Interval (0) -> Field not presented
+     * <br>Unused (000)
+     *
+     * @param heartRateMeasurement (bpm) heart rate measurement value in UInt8/UInt16 format
+     * @param expendedEnergy       (kiloJoules) expended energy value in UInt16 format
+     * @throws GattException in case of wrong arguments format or offset for heart rate characteristic values
+     */
+    public void setHeartRateCharacteristicValue(int heartRateMeasurement, int expendedEnergy) throws GattException {
+        Log.i(TAG, String.format("Set the heart measurements value %d and expended energy value %d",
+                heartRateMeasurement, expendedEnergy));
+
+        FormatUtils.assertIsUInt16(heartRateMeasurement);
+        FormatUtils.assertIsUInt16(expendedEnergy);
+
+        if (FormatUtils.isInUInt8Range(heartRateMeasurement)) {
+
+            /* Flags(UInt8) + HR(UInt8) + EE(UInt16) */
+            setValue(new byte[4]);
+
+            /* EE present and HR UInt8 format => Flags = 0b00001000 */
+            setIntValue((byte) (HEART_RATE_MEASUREMENT_UINT8_FLAG | EXPENDED_ENERGY_FLAG),
+                    BluetoothGattCharacteristic.FORMAT_UINT8,
+                    FLAGS_OFFSET);
+
+            setIntValue(heartRateMeasurement,
+                    BluetoothGattCharacteristic.FORMAT_UINT8,
+                    HEART_RATE_MEASUREMENT_OFFSET);
+
+            /* HR is in UInt8 format => EE offset is base */
+            setIntValue(expendedEnergy,
+                    BluetoothGattCharacteristic.FORMAT_UINT16,
+                    EXPENDED_ENERGY_BASE_OFFSET);
+        } else {
+
+            /* Flags(UInt8) + HR(UInt16) + EE(UInt16) */
+            setValue(new byte[5]);
+
+            /* EE present and HR UInt16 format => Flags = 0b00001001 */
+            setIntValue((byte) (HEART_RATE_MEASUREMENT_UINT16_FLAG | EXPENDED_ENERGY_FLAG),
+                    BluetoothGattCharacteristic.FORMAT_UINT8,
+                    FLAGS_OFFSET);
+
+            setIntValue(heartRateMeasurement,
+                    BluetoothGattCharacteristic.FORMAT_UINT16,
+                    HEART_RATE_MEASUREMENT_OFFSET);
+
+            /* HR is in UInt16 format => EE offset is shifted */
+            setIntValue(expendedEnergy,
+                    BluetoothGattCharacteristic.FORMAT_UINT16,
+                    EXPENDED_ENERGY_SHIFTED_OFFSET);
+        }
+    }
+
+    /**
+     * Set given Heart Rate Measurement value to the characteristic.
+     * <p>
+     * Flags 8bit format 0b00000000 from right to left:
+     * <br>Heart Rate Format (0) -> UInt8 Format, (1) -> UInt16 Format
+     * <br>Sensor Contact Status (00) -> Not Supported
+     * <br>Energy Expended (0) -> Field not Present
+     * <br>RR-Interval (0) -> Field not presented
+     * <br>Unused (000)
+     *
+     * @param heartRateMeasurement (bpm) heart rate measurement value in UInt8/UInt16 format
+     * @throws GattException in case of wrong arguments format or offset for heart rate characteristic values
+     */
+    public void setHeartRateCharacteristicValue(int heartRateMeasurement) throws GattException {
+        Log.i(TAG, String.format("Set the heart measurements value %d", heartRateMeasurement));
+
+        FormatUtils.assertIsUInt16(heartRateMeasurement);
+
+        if (FormatUtils.isInUInt8Range(heartRateMeasurement)) {
+
+            /* Flags(UInt8) + HR(UInt8) */
+            setValue(new byte[2]);
+
+            /* EE not present and HR UInt8 format => Flags = 0b00000000 */
+            setIntValue(HEART_RATE_MEASUREMENT_UINT8_FLAG,
+                    BluetoothGattCharacteristic.FORMAT_UINT8,
+                    FLAGS_OFFSET);
+
+            setIntValue(heartRateMeasurement,
+                    BluetoothGattCharacteristic.FORMAT_UINT8,
+                    HEART_RATE_MEASUREMENT_OFFSET);
+
+        } else {
+
+            /* Flags(UInt8) + HR(UInt16) */
+            setValue(new byte[3]);
+
+            /* EE not present and HR UInt16 format => Flags = 0b00000001 */
+            setIntValue(HEART_RATE_MEASUREMENT_UINT16_FLAG,
+                    BluetoothGattCharacteristic.FORMAT_UINT8,
+                    FLAGS_OFFSET);
+
+            setIntValue(heartRateMeasurement,
+                    BluetoothGattCharacteristic.FORMAT_UINT16,
+                    HEART_RATE_MEASUREMENT_OFFSET);
+        }
+    }
+
+    /**
+     * Assert Expended Energy is supported.
+     *
+     * @throws GattException if not supported
+     */
+    private void assertExpandedEnergyIsPresented() throws GattException {
+        if (!isExpandedEnergyPresent()) {
+            Log.w(TAG, "Expended energy value is not presented");
+            throw new GattException("Expended energy value is not presented",
+                    BluetoothGatt.GATT_REQUEST_NOT_SUPPORTED);
+        }
+    }
+
+    /**
+     * Determine whether the Heart Rate value is in UInt16 format or not.
+     * <br>If Heart Rate Value Format bit in Flags Field is 1, then UInt16 format, otherwise UInt8.
+     *
+     * @return true is UInt16, false if UInt8
+     */
+    private boolean isUInt16HeartRateFormat() throws GattException {
+        int flags = getIntValue(
+                BluetoothGattCharacteristic.FORMAT_UINT8,
+                FLAGS_OFFSET);
+        return (flags & HEART_RATE_MEASUREMENT_UINT16_FLAG) > 0;
+    }
+
+    /**
+     * Determine whether the Energy Expended Field is present or not.
+     * If Energy Expended Status bit in Flags Field is 1, then is present, otherwise not.
+     *
+     * @return true is UInt16, false if UInt8
+     */
+    private boolean isExpandedEnergyPresent() throws GattException {
+        int flags = getIntValue(
+                BluetoothGattCharacteristic.FORMAT_UINT8,
+                FLAGS_OFFSET);
+        return (flags & EXPENDED_ENERGY_FLAG) > 0;
+    }
+}
