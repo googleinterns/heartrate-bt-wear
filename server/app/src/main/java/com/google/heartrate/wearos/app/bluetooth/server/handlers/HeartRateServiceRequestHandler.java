a55ed47c887c2a825395d055a0d55b7426758c28
tiginamaria <tiginamaria@google.com>
2020-08-06 19:08:00 +0300

    Add heart rate bluetooth server implementation

---
+++ server/app/src/main/java/com/google/heartrate/wearos/app/bluetooth/server/handlers/HeartRateServiceRequestHandler.java
@@ -0,0 +1,143 @@
+package com.google.heartrate.wearos.app.bluetooth.server.handlers;
+
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothGattService;
+import android.util.Log;
+
+import com.google.heartrate.wearos.app.bluetooth.server.BluetoothDeviceStorage;
+import com.google.heartrate.wearos.app.bluetooth.server.BluetoothServerCallback;
+import com.google.heartrate.wearos.app.gatt.GattException;
+import com.google.heartrate.wearos.app.gatt.attributes.GattService;
+import com.google.heartrate.wearos.app.gatt.heartrate.characteristics.HeartRateMeasurementCharacteristic;
+import com.google.heartrate.wearos.app.gatt.heartrate.service.HeartRateGattService;
+import com.google.heartrate.wearos.app.sensors.HeartRateSensorListener;
+import com.google.heartrate.wearos.app.sensors.HeartRateValueSubscriber;
+
+import java.util.Arrays;
+
+/**
+ * {@link GattServiceRequestHandler} for Heart Rate service.
+ *
+ * <p>Provides methods to handle request from remote devices to Heart Rate service.
+ */
+public class HeartRateServiceRequestHandler implements GattServiceRequestHandler, HeartRateValueSubscriber {
+    private static final String TAG = HeartRateServiceRequestHandler.class.getSimpleName();
+
+    /** {@link GattService} for Heart Rate service. */
+    private final HeartRateGattService mHeartRateGattService;
+
+    /** Sensor listener for heart rate sensor. */
+    private final HeartRateSensorListener mHeartRateSensorListener;
+
+    /** Server callback to interact with remote device. */
+    private BluetoothServerCallback mBluetoothServerCallback;
+
+    /** Storage for all devices registered to Heart Rate Measurement characteristic. */
+    private BluetoothDeviceStorage mRegisteredDevices;
+
+    public HeartRateServiceRequestHandler(HeartRateSensorListener heartRateSensorListener) {
+        mHeartRateSensorListener = heartRateSensorListener;
+        mHeartRateGattService = new HeartRateGattService();
+        mRegisteredDevices = new BluetoothDeviceStorage();
+    }
+
+    /**
+     * {@link GattServiceRequestHandler#onServiceAdded}
+     * <p>Register to {@link HeartRateSensorListener} to receive heart rate from sensor.
+     */
+    @Override
+    public void onServiceAdded(BluetoothServerCallback bluetoothServerCallback) {
+        mBluetoothServerCallback = bluetoothServerCallback;
+        mHeartRateSensorListener.registerSubscriber(this);
+    }
+
+    /**
+     * {@link GattServiceRequestHandler#onServiceAdded}
+     * <p>Unregister to {@link HeartRateSensorListener}.
+     */
+    @Override
+    public void onServiceRemoved() {
+        mHeartRateSensorListener.unregisterSubscriber(this);
+        mRegisteredDevices.removeAllDevices();
+    }
+
+    /**
+     * Unregister device from notifications about heart rate characteristic changed.
+     * @param device connected device
+     */
+    @Override
+    public void onDeviceDisconnected(BluetoothDevice device) {
+        mRegisteredDevices.removeDevice(device);
+    }
+
+    /**
+     * Determine wether remote device is registered for notifications about Heart Rate Measurement characteristic change or not.
+     *
+     * <p>Read descriptor request for Heart Rate service is the way for remote device to check status of registration
+     * to Heart Rate Measurement characteristic.
+     *
+     * @param device the remote device that has requested the read operation
+     * @param descriptor descriptor to be read
+     * @param offset offset into the value of the descriptor
+     * @return {@link BluetoothGattDescriptor#ENABLE_NOTIFICATION_VALUE} if device is registered for notifications,
+     * {@link BluetoothGattDescriptor#DISABLE_NOTIFICATION_VALUE} otherwise
+     */
+    @Override
+    public byte[] onDescriptorRead(BluetoothDevice device, BluetoothGattDescriptor descriptor, int offset) {
+        if (mRegisteredDevices.contains(device)) {
+            return BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE;
+        } else {
+            return BluetoothGattDescriptor.DISABLE_NOTIFICATION_VALUE;
+        }
+    }
+
+    /**
+     * Register (if given value is {@link BluetoothGattDescriptor#ENABLE_NOTIFICATION_VALUE}) or
+     * unregister (if given value is {@link BluetoothGattDescriptor#DISABLE_NOTIFICATION_VALUE})
+     * remote device for notifications about Heart Rate Measurement characteristic change.
+     *
+     * <p>Write descriptor request for Heart Rate service is the way for remote device to subscribe/unsubscribe
+     * for Heart Rate Measurement characteristic change notification.
+     *
+     * @param device the remote device that has requested the read operation
+     * @param descriptor descriptor to be read
+     * @param offset offset into the value of the descriptor
+     */
+    @Override
+    public void onDescriptorWrite(BluetoothDevice device, BluetoothGattDescriptor descriptor, int offset, byte[] value) {
+        if (Arrays.equals(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE, value)) {
+            Log.d(TAG, String.format("Subscribe device %s to notifications", device));
+            mRegisteredDevices.addDevice(device);
+        } else if (Arrays.equals(BluetoothGattDescriptor.DISABLE_NOTIFICATION_VALUE, value)) {
+            Log.d(TAG, String.format("Unsubscribe device %s from notifications", device));
+            mRegisteredDevices.removeDevice(device);
+        }
+    }
+
+    /**
+     * Callback to listen to {@link HeartRateSensorListener} about heart rate value change.
+     * <p>Update Heart Rate measurement characteristic value with given and send notification about
+     * Heart Rate measurement characteristic change to server via {@link BluetoothServerCallback}
+     * @param value new heart rate value
+     */
+    @Override
+    public void onHeartRateValueChanged(int value) {
+        try {
+            HeartRateMeasurementCharacteristic heartRateMeasurementCharacteristic
+                = mHeartRateGattService.getHeartRateMeasurementCharacteristic();
+            heartRateMeasurementCharacteristic.setHeartRateCharacteristicValue(value);
+
+            mBluetoothServerCallback.onCharacteristicChanged(heartRateMeasurementCharacteristic.getBluetoothGattCharacteristic(),
+                    mRegisteredDevices.getAllDevices());
+
+        } catch (GattException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public BluetoothGattService getBluetoothGattService() {
+        return mHeartRateGattService.getBluetoothGattService();
+    }
+}
