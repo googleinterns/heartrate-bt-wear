cee2c4dcad6cbafaf152f25f8396de8eb9193381
tiginamaria <tiginamaria@google.com>
2020-08-21 16:31:34 +0300

    Fix execution

--- server/app/src/main/java/com/google/heartrate/wearos/app/bluetooth/server/handlers/HeartRateServiceRequestHandler.java
+++ server/app/src/main/java/com/google/heartrate/wearos/app/bluetooth/server/handlers/HeartRateServiceRequestHandler.java
@@ -15,6 +15,7 @@ import com.google.heartrate.wearos.app.sensors.HeartRateSensorListener;
 import com.google.heartrate.wearos.app.sensors.HeartRateValueSubscriber;
 
 import java.util.Arrays;
+import java.util.Optional;
 
 /**
  * {@link GattServiceRequestHandler} for Heart Rate service.
